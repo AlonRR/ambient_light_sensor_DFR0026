65cdfc5f2bed41fb203031f2f3e870d0c71e8f8e
Alon A. Rabinowitz <alonrab@outlook.com>
2025-01-27 12:17:05 +0200

    added analogRead on the sensor pin and prints the value.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,16 @@
 #include <Arduino.h>
 
+#define AMBIENT_LIGHT_PIN 25
+
 void setup()
 {
     Serial.begin(115200);
+    pinMode(AMBIENT_LIGHT_PIN, INPUT);
 }
 
 void loop()
 {
+    int lightValue = analogRead(AMBIENT_LIGHT_PIN);
+    Serial.println(lightValue);
+    delay(1000);
 }
